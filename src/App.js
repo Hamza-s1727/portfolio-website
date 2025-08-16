5db6a9afde401b9805c44a05abf61b7c9bd9b845
sheik418 <hamzaa.sheikh@mail.utoronto.ca>
2025-08-16 01:48:50 -0400

    Added Header functionality

--- src/App.js
+++ src/App.js
@@ -6,16 +6,34 @@ import Skills from "./components/Skills"
 import Projects from './components/Projects';
 import Contact from "./components/Contact"
 
+import { useRef } from "react"
+
 
 function App() {
+
+  const welcomeRef = useRef(null);
+  const aboutRef = useRef(null);
+  const skillsRef = useRef(null);
+  const projectsRef = useRef(null);
+  const contactRef = useRef(null);
+  
+
+
+  function scrollToSection(ref) {
+    if (ref.current) {
+      ref.current.scrollIntoView({behavior: "smooth"})
+    }
+  }
+
+
   return (
     <div className="App">
-      <Header />
-      <Welcome />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
+      <Header refs={{welcomeRef, aboutRef, skillsRef, projectsRef, contactRef}} scrollToSection={scrollToSection}/>
+      <div ref={welcomeRef}> <Welcome /> </div>
+      <div ref={aboutRef}><About /> </div>
+      <div ref={skillsRef}> <Skills /> </div>
+      <div ref={projectsRef}> <Projects /> </div>
+      <div ref={contactRef}> <Contact /> </div>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import "./Header.css"
 import React from "react"
-export default function Header() {
+export default function Header(props) {
 
     const [navbarOpen, setNavBarOpen] = React.useState(false)
     React.useEffect(() => {
@@ -26,7 +26,6 @@ export default function Header() {
 
 
     function ToggleNavBar() {
-        console.log("its working")
         setNavBarOpen(prevState => !prevState)
     }
 
@@ -36,22 +35,22 @@ export default function Header() {
         <header>
             <h2>Hamza Sheikh</h2>
             <div className="header-buttons">
-                <button className="headerButton">Home</button>
-                <button className="headerButton">About</button>
-                <button className="headerButton">Skills</button>
-                <button className="headerButton">Projects</button>
-                <button className="headerButton">Contact</button>
+                <button className="headerButton" onClick={() => props.scrollToSection(props.refs.welcomeRef)} >Home</button>
+                <button className="headerButton"onClick={() => props.scrollToSection(props.refs.aboutRef)} >About</button>
+                <button className="headerButton" onClick={() => props.scrollToSection(props.refs.skillsRef)}>Skills</button>
+                <button className="headerButton" onClick={() => props.scrollToSection(props.refs.projectsRef)}>Projects</button>
+                <button className="headerButton" onClick={() => props.scrollToSection(props.refs.contactRef)}>Contact</button>
             </div>
             <div className="MobileNavBar">
                 <button className="NavBarButton" onClick={ToggleNavBar}></button>
             </div>
         </header>
         <div className="NavBarExpanded" style={navMenuStyle}>
-                    <button className="headerButton">Home</button>
-                    <button className="headerButton">About</button>
-                    <button className="headerButton">Skills</button>
-                    <button className="headerButton">Projects</button>
-                    <button className="headerButton">Contact</button>
+                    <button className="headerButton" onClick={() => props.scrollToSection(props.refs.welcomeRef)}>Home</button>
+                    <button className="headerButton" onClick={() => props.scrollToSection(props.refs.aboutRef)}>About</button>
+                    <button className="headerButton" onClick={() => props.scrollToSection(props.refs.skillsRef)}>Skills</button>
+                    <button className="headerButton" onClick={() => props.scrollToSection(props.refs.projectsRef)}>Projects</button>
+                    <button className="headerButton" onClick={() => props.scrollToSection(props.refs.contactRef)}>Contact</button>
         </div>
         </>
     )
